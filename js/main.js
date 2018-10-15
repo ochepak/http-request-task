c83f641dc6354ae459092b08a20dad150f6d4bd8
Oleh Chepak <o.chepak@outlook.com>
2018-10-15 17:11:10 +0300

    added ajax request

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+
+    ajaxRequest(data => {
+        console.log('\nAjax request:');
+        console.log(data);
+    });
+
+    function ajaxRequest(callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    callback(JSON.parse(xhr.responseText));
+                } else {
+                    console.error('Ajax request FAILED\n' + xhr.status + ": " + xhr.statusText);
+                };
+            };
+        };
+        try {
+            xhr.open('GET', 'https://api.github.com/users');
+            xhr.send();
+        } catch (error) {
+            xhr.abort();
+            console.error(error);
+        }
+    }
+}
